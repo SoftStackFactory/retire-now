450cb8c3dc13cc7948e06f551f0bac9c7a014d40
Khoa Nguen <visthetic@gmail.com>
2018-07-10 22:33:54 -0700

    Testing build process and deploy

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 'use strict';
 
-const path = require('path');
-const express = require('express');
-const http = require('http');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 3000
 
-const app = express();
-const server = http.Server(app);
-
-const port = process.env.PORT || 8080;
-
-app.use('/', express.static(path.join(__dirname, 'www')));
-
-server.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}/`);
-});
+express()
+  .use(express.static(path.join(__dirname, 'www')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
