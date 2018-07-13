fc37d2912189e025541223c1663ad2437f1283ee
Khoa Nguen <visthetic@gmail.com>
2018-07-12 21:13:30 -0700

    Added page structure, added navigation

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,37 +1,68 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+const items = [
+  {
+    name: 'Landing Page',
+    component: 'LandingPage',
+    note: 'This is the landing page',
+    icon: 'analytics'
+  },
+  {
+    name: 'Login Page',
+    component: 'LoginPage',
+    note: 'This is the login page',
+    icon: 'add'
+  },
+  {
+    name: 'Register Page',
+    component: 'RegisterPage',
+    note: 'This is the register page',
+    icon: 'barcode'
+  },
+  {
+    name: 'Tutorial Page',
+    component: 'TutorialPage',
+    note: 'This is the tutorial page',
+    icon: 'bowtie'
+  },
+  {
+    name: 'Input Page',
+    component: 'InputPage',
+    note: 'This is the input page',
+    icon: 'bulb'
+  },
+  {
+    name: 'Results Page',
+    component: 'ResultsPage',
+    note: 'This is the results page',
+    icon: 'cafe'
+  },
+  {
+    name: 'DashboardPage',
+    component: 'DashboardPage',
+    note: 'This is the dashboard page',
+    icon: 'chatbubbles'
+  },
+  {
+    name: 'Account Page',
+    component: 'AccountPage',
+    note: 'This is the account page',
+    icon: 'cloudy'
+  },
+]
+
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  pages: Array<{name: string, component: string, note: string, icon: string}> = items;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+  itemTapped(event, page) {
+    this.navCtrl.push(page);
   }
 }
