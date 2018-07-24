1807568ddc8f64964a7ef619fc2b43b7354453bc
lmccraw123 <laurenmccraw@gmail.com>
2018-07-23 22:11:08 -0500

    cleaned up .html

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Validators, FormBuilder, FormGroup } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 
 /**
