67a9c6adb02c166ac3add806d22e806db6bbe902
lmccraw123 <laurenmccraw@gmail.com>
2018-08-12 22:30:30 -0500

    register button is invalid unless the form is complete

--- src/components/password-validation/password-validation.ts
+++ src/components/password-validation/password-validation.ts
@@ -22,10 +22,10 @@ export class PasswordValidationComponent {
 
   static MatchPassword(AC: AbstractControl) {
     let password = AC.get('password').value; // to get value in input tag
-    let confirmPassword = AC.get('confirmPassword').value; // to get value in input tag
-     if(password != confirmPassword) {
+    let vpassword = AC.get('vpassword').value; // to get value in input tag
+     if(password != vpassword) {
          console.log('false');
-         AC.get('confirmPassword').setErrors( {MatchPassword: true} )
+         AC.get('vpassword').setErrors( {MatchPassword: true} )
      } else {
          console.log('true');
          return null
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {Validators, FormBuilder, FormGroup} from '@angular/forms';
 import {UserProvider} from '../../providers/user/user';
-import { PasswordValidation } from '../../components/password-validation/password-validation';
+import { PasswordValidationComponent } from '../../components/password-validation/password-validation';
 
 /**
  * Generated class for the RegisterPage page.
@@ -19,8 +19,9 @@ import { PasswordValidation } from '../../components/password-validation/passwor
 export class RegisterPage {
 
   registerInput: FormGroup;
-  user = {vpassword: '',
-          password: ''};
+  user = {password:"",
+          vpassword:''
+};
   error = {message: ''};
 
   constructor(
@@ -32,29 +33,14 @@ export class RegisterPage {
       first: ['', Validators.required],
       last: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', this.matchPasswords],
+      password: ['',Validators.required],
       vpassword: ['', Validators.required],
       dob: ['', Validators.required],
-    }, {validator: this.matchPasswords('password', 'vpassword')});
+    }, {validator: [PasswordValidationComponent.MatchPassword],
+
+    });
     
   }
-
-  matchPasswords(passwordKey: any, vpasswordKey: string){
-    return(group: FormGroup): {[key:string]: any} => {
-      let password = group.controls[passwordKey];
-      let vpassword = group.controls[vpasswordKey];
-
-      if (password.value !== vpassword.value) {
-        return {mismatchedPasswords: true};
-      }
-    }
-  }
-
-
-  //[Validators.pattern('^[a-zA-Z0-9._-](?=.*[!@#\$%\^&\*]).{6,8}$') in case needed use this.
-//   userName: ['', [Validators.required, Validators.email]],
-//   passWord: ['', [Validators.pattern('^[a-zA-Z0-9._-](?=.*[!@#\$%\^&\*]).{6,8}$'), Validators.required]],
-// });
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
@@ -63,16 +49,19 @@ export class RegisterPage {
   //form builder function that allows for register input to become validated
   onForm() {
     console.log(this.registerInput.value)
+ 
   }
 
   //function to register a user
   onRegister() {
+    console.log("PW", this.registerInput.status == "INVALID")
     console.log(this.user);
     this._user.onReg(this.user)
       .subscribe((res: any) => {
         sessionStorage.setItem('token', res.token);
         sessionStorage.setItem('userId', res.userId);
-        this.navCtrl.push('TutorialPage');
+          this.navCtrl.push('TutorialPage');
+
       }, (error: any) => {
         if (error.status === 401) {
           console.log('Error Message:', error.message)
